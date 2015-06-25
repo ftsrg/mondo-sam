64853f608489d6530d132fc497baa27ac03d3d01
Zsolt Kővári <kovari.zsolt91@gmail.com>
2015-06-25 11:10:39 +0200

    Update install.R

--- reporting/shiny/install.R
+++ reporting/shiny/install.R
@@ -1,2 +1,1 @@
-install.packages("shiny", repos="http://cran.rstudio.com/", dependencies = TRUE);
-install.packages("R.oo", repos="http://cran.rstudio.com/", dependencies = TRUE);
+install.packages("shiny", repos="http://cran.rstudio.com/", dependencies = TRUE)
