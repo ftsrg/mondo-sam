2403f9ed0fc06a89aba010e990600a953b093737
Gabor Szarnyas <szarnyasg@gmail.com>
2016-01-20 10:47:15 +0100

    Updated install.R script

--- reporting/install.R
+++ reporting/install.R
@@ -2,4 +2,5 @@ install.packages("ggplot2", repos="http://cran.rstudio.com/")
 install.packages("jsonlite", repos="http://cran.rstudio.com/")
 install.packages("R.oo", repos="http://cran.rstudio.com/", dependencies = TRUE)
 install.packages("plyr", repos = "http://cran.rstudio.com/")
-
+install.packages("xtable", repos = "http://cran.rstudio.com/")
+install.packages("knitr", repos = "http://cran.rstudio.com/")
